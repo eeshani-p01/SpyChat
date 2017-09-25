c31125d7375c8d384200f4884b0c35ddbae95c10
Eeshani Patel <eeshani.p01@gmail.com>
2017-09-25 23:43:43 +0530

    Intro to spyChat

--- spyChat.py
+++ spyChat.py
@@ -1,1 +1,5 @@
 print "\tWelcome to SpyChat"
+spy_name = raw_input("What is your name? \n")
+salu = raw_input("What should I call you, Mr. or Mrs.? \n")
+spy_name = salu+ spy_name
+print "Hello, " + spy_name
