867d38ba03c1352d32be56ed102f3ebeaacb9a59
Eeshani Patel <eeshani.p01@gmail.com>
2017-09-26 01:38:18 +0530

    Import default values

--- spyChat.py
+++ spyChat.py
@@ -1,29 +1,43 @@
-print "\tWelcome to SpyChat"
-spy_name = raw_input("What is your name? \n")
-salu = raw_input("What should I call you, Mr. or Mrs.? \n")
-spy_name = salu+" "+ spy_name
-print "Hello, " + spy_name
+from spy_details import spy_name,salu,spy_age,spy_rating
 
-spy_age = 0
-spy_rating = 0.0
-spy_is_online = False
+print "Welcome to SpyChat!!"
+question = "Want to Continue as " + salu + " " + spy_name + "(Y/N)?"
+print question
+user_input = raw_input("Enter your choice\n")
 
-spy_age=input("What is your age? ")
-if spy_age > 12 and spy_age < 50 :
-    spy_rating = input("What is your spy rating ? ")
-else :
-    print "Sorry, Your age doesn't fit to be a spy "
+if user_input == 'Y':
+    #normal operation
+    pass    # a keyword to just pass the execution
 
-if spy_rating > 4.5:
-    print 'Great ace!'
-elif spy_rating > 3.5 and spy_rating <= 4.5:
-    print 'You are one of the good ones.'
-elif spy_rating >= 2.5 and spy_rating <= 3.5:
-    print 'You can always do better'
 else:
-    print 'We can always use somebody to help in the office.'
+    spy_name = raw_input("Hello dear ! Tell me your spy name :  \n")
+    if len(spy_name)>0 :
+        salu = raw_input("What should I call you, Mr. or Mrs.? \n")
+        spy_name = salu+" "+ spy_name
+        print "Hello, " + spy_name
 
-spy_is_online = True
+        spy_age = 0
+        spy_rating = 0.0
+        spy_is_online = False
 
-print "Authentication complete.  Welcome %s  age: %d and rating of: %1f Proud to have you onboard" %(spy_name,spy_age,spy_rating)
+        spy_age=input("What is your age? ")
+        if spy_age > 12 and spy_age < 50 :
+            spy_rating = input("What is your spy rating ? ")
+        else :
+            print "Sorry, Your age doesn't fit to be a spy "
 
+        if spy_rating > 4.5:
+            print 'Great ace!'
+        elif spy_rating > 3.5 and spy_rating <= 4.5:
+            print 'You are one of the good ones.'
+        elif spy_rating >= 2.5 and spy_rating <= 3.5:
+            print 'You can always do better'
+        else:
+            print 'We can always use somebody to help in the office.'
+
+        spy_is_online = True
+
+        print "Authentication complete.  Welcome %s  age: %d and rating of: %1f Proud to have you onboard" %(spy_name,spy_age,spy_rating)
+
+    else:
+        print "This spy doesn't exist !!"
---
+++ spy_details.py
@@ -0,0 +1,4 @@
+spy_name = 'Bond'
+salu = 'Dr.'
+spy_age = 23
+spy_rating = 4.7
