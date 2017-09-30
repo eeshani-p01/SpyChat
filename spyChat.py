8a404af99f9d113796a0b73bea8a8c68eb326497
Eeshani Patel <eeshani.p01@gmail.com>
2017-09-30 16:56:36 +0530

    Friend functionality added

--- spyChat.py
+++ spyChat.py
@@ -1,8 +1,14 @@
 from spy_details import spy_name,salu,spy_age,spy_rating
 import sys #system module
 
+STATUS_MESSAGES = ['My name is Bond, James Bond', 'Shaken, not stirred.']
+friend_name=[]
+friend_age=[]
+friend_rating=[]
+friend_is_online=[]
+
 print "Welcome to SpyChat!!"
-question = "Want to Continue as " + salu + " " + spy_name + "(Y/N)?"
+question = "Do you want to continue as " + salu + " " + spy_name + "(Y/N)?"
 print question
 user_input = raw_input("Enter your choice\n")
 
@@ -11,16 +17,19 @@ def start_chat(spy_name,spy_age,spy_rating):
     show_menu = True
 
     while show_menu:
-        print "What do you want to do? \n1. Add a status update \n2. Close Application\n"
+        print "What do you want to do? \n1. Add a status update \n2. Add a friend\n 3. Close Application\n"
         menu_choice = input("Enter your choice\n")
 
         if menu_choice == 1:
             print 'You chose to update the status'
             current_status=add_status(current_status)
-        elif  menu_choice == 2:
+        elif menu_choice == 2:
+            add_friend()
+        elif  menu_choice == 3:
             show_menu = False
+        else
+            sys.exit()
 
-STATUS_MESSAGES = ['My name is Bond, James Bond', 'Shaken, not stirred.']
 
 def add_status(current_status):
     if current_status !=None:
@@ -45,7 +54,26 @@ def add_status(current_status):
         if len(STATUS_MESSAGES)>=message_select:
             updated_status_message = STATUS_MESSAGES[message_select-1]
             print "Your status is updated."
+        else:
+            print "You didn't enter any status!!!"
     return updated_status_message
+
+def add_friend():
+    new_name = input("Please enter your friend's name:")
+    new_salutation = input("Are they Mr. or Ms.?: ")
+    new_name = new_name + " " + new_salutation
+    new_age = input("What's the age?")
+    new_rating = input("What is their Spy rating?")
+    if len(new_name)>0 and new_age>12 and new_rating>=spy_rating:
+        friend_name.append(new_name)
+        friend_age.append(new_age)
+        friend_rating.append(new_rating)
+        friend_is_online.append(True)
+    else:
+        print "Sorry ! but we can't add this spy to your friend list. "
+    return len(friend_name)
+
+
 
 if user_input == 'Y':
     #normal operation
