333dce6734fd6f45346cc55ddf9ddd91135c2e04
Eeshani Patel <eeshani.p01@gmail.com>
2017-10-03 19:45:34 +0530

    Spy class adde

--- spy_details.py
+++ spy_details.py
@@ -1,7 +1,17 @@
-spy = {
-    'name' : 'Bond',
-    'salu' : 'Dr.',
-    'age' : 23,
-    'rating' : 4.7,
-    'is_online' : True
-}
+# # spy = {
+#     'name' : 'Bond',
+#     'salu' : 'Dr.',
+#     'age' : 23,
+#     'rating' : 4.7,
+#     'is_online' : True
+# }
+
+class Spy:
+    def __init__(self, name, salu, age, rating):
+        self.name = name
+        self.salu = salu
+        self.age = age
+        self.rating =rating
+        self.is_online = True
+        self.chats = []
+        self.current_status = None
